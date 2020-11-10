c0e552ef6af05e6f27c11dcd28becaccf0f689ff
fllkai <65815642+fllkai@users.noreply.github.com>
2020-11-10 23:40:52 +0800

    Update AddDynamics.js

--- pages/showDynamic/AddDynamics.js
+++ pages/showDynamic/AddDynamics.js
@@ -31,37 +31,7 @@ Page({
     note_type: 1,
     note_content:"",
   },
-  downLoad:function(){
-    let that = this 
-    wx.downloadFile({
-      url:that.data.code,
-      header:{},
-      success:function(res){
-        that.setData({ tempFileCode: res.tempFilePath})
-      }
-    })
-  },
  
-  showSharePic:function(){
-    var that = this
-    wx.request({
-      url: app.globalData.urls+'share',
-      data:{
-        note_id:that.data.note_id,
-        page:'pages/showDynamic/AddDyynamics'
-      },
-      success:function(res){
-         var base64 = res.data
-         console.log(base64.url)
-        that.setData({imgUrl:base64.url})
-        console.log(res)
-        that.savePhoto()
-      },
-      fail:function(res){
-        console.log(res)
-      }
-    })
-  },
   likeNote:function(){
     var that = this
     if (that.data.state==0)
@@ -72,7 +42,6 @@ Page({
         note_id: that.data.note_id
       },
       success:function(res){
-        console.log("我是点赞成功的反馈",res)
         that.setData({
           state:1,
           note_likes:that.data.note_likes+1
@@ -113,7 +82,6 @@ Page({
     })
   },
   onLoad: function (options) {
-    // console.log("我是跳转后的结果",options)
     if (app.globalData.usrId == 2415125)
       this.checkInfo(options)
     let imgs = ""
@@ -179,7 +147,6 @@ Page({
       },
 
       success: function (res) {
-        console.log("我是评论内容", res)
         if (res.data[0].code != 0) {
           that.setData({
             comment_list: res.data
@@ -192,10 +159,8 @@ Page({
     var that = this
     wx.getSetting({
       success(res) {
-        // console.log("res", res)
         if (res.authSetting['scope.userInfo']) {
           console.log("已授权=====")
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
           wx.getUserInfo({
             lang: "zh_CN",
             success(res) {
@@ -284,22 +249,6 @@ Page({
       status: contentRe
     })
   },
-  DelImg(e) {
-    wx.showModal({
-      title: '记忆回收站',
-      content: '确定要删除这个照片吗？',
-      cancelText: '再看看',
-      confirmText: '删除',
-      success: res => {
-        if (res.confirm) {
-          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
-          this.setData({
-            imgList: this.data.imgList
-          })
-        }
-      }
-    })
-  },
   followUser:function(res){
     var that = this
     // console.log("我是被关注用户", this.data.user_id)
@@ -320,7 +269,6 @@ Page({
   },
   unFollowUser:function(res){
     var that = this
-    // console.log("我是被关注用户", this.data.user_id)
     wx.request({
       url: app.globalData.urls + 'unfollow',
       data: {
@@ -361,20 +309,19 @@ Page({
         console.log(i);
       },
       fail: (res) => {
-        fail++;//图片上传失败，图片上传失败的变量+1
-        console.log('fail:' + i + "fail:" + fail, "失败原因是:", res);
+        fail++;
       },
       complete: () => {
         console.log(i);
-        i++;//这个图片执行完上传后，开始上传下一张            
-        if (i - 1 == data.path.length) {   //当图片传完时，停止调用          
+        i++;           
+        if (i - 1 == data.path.length) {           
           console.log('执行完毕');
           console.log('成功：' + success + " 失败：" + fail);
           wx.hideLoading()
           wx.navigateBack({
             delta: 1
           })
-        } else {//若图片还没有传完，则继续调用函数                
+        } else {             
           console.log(i);
           data.i = i;
           data.success = success;
@@ -438,42 +385,6 @@ Page({
           that.uploadVideo({ path: that.data.upload_video_show })
       }
     })
-  },
-  submit: function (e) {
-    console.log(e)
-    var that = this;
-    wx.uploadFile({
-      url: 'http://47.97.219.21/addNote', 
-      filePath: that.data.imgList[0],
-      name: 'note_pic',
-      formData: {
-        'note_hide': 0,
-        'note_time': "2019-11-15 15:37:00",
-        'user_id': 1,
-        'note_content': "test",
-        'note_title': "sdsd",
-        'sessionKey': "2415125",
-      },
-      success(res) {
-        const data = res.data
-        console.log(res)
-        //do something
-      },
-      fail(res) {
-        console.log(res)
-      }
-    })
-    wx.showToast({
-      title: '发布成功',
-      icon: 'success',
-      duration: 1500,
-      mask: false,
-    })
-    setTimeout(function () {
-      wx.navigateBack({
-        delta: 1
-      })
-    }, 1500)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
